99be3ba1d9ed2cac0f2eddd825b6934f44386749
williamsiewe16 <loicosiewe16@gmail.com>
2023-01-03 04:42:44 +0100

    yamnet tflite model integration

--- iot.py
+++ iot.py
@@ -29,7 +29,7 @@ try:
         print("\n",f"---------------- Frames sequence N°{val}")
         frames = []
         for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-            data = stream.read(CHUNK)
+            data = stream.read(CHUNK, exception_on_overflow=False)
             frames.append(data)
 
         print("* done recording ")
